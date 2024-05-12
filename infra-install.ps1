8ebe6f22a6943ea2e65a1bc19b8ba5320a16d631
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-05-12 15:23:59 +0300

    Update infra-install.ps1

--- infra-install.ps1
+++ infra-install.ps1
@@ -1,5 +1,10 @@
-
-
+param(
+[string]$a,
+[string]$b
+)
+     
+     
+docker rm -f jenkins-auto
 docker run --name jenkins-auto -d -p 8080:8080 -p 50000:50000 --restart=on-failure -v //var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home boazhalter/gm:jenkins-latest
-docker exec jenkins-auto java -jar jenkins-cli.jar -s http://localhost:8080 -auth $args[0]:$args[1] reload-jcasc-configuration
-chmod a+rw /var/run/docker.sock
+Start-Sleep -Seconds 20
+docker exec jenkins-auto java -jar jenkins-cli.jar -s http://localhost:8080 -auth ${a}:${b} reload-jcasc-configuration
